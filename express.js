d95cdd6ccbb644a7f40aa3f31f14a580cfed1979
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-08-07 01:15:27 +0530

    Delete express.js

--- express.js
+++
@@ -1,9 +0,0 @@
-const { response } = require("express");
-const express = require("express");
-const app  =express();
-app.get("/",function(req,resp){resp.send("<h1>hello<h1>");});
-app.get("/contact",function(req,resp){resp.send("<p>contact me at <i>hegdenandan013@gmail.com<i><p>");});
-app.get("/about",function(req,resp){resp.send("<p>My Name is <i>:Nandan Hegde<i><p>");});
-app.get("/hobbies",function(req,resp){resp.send("<p>My hobby is <i>:Coding<i><p>");});
-
-app.listen(3000,function(){console.log("server started at 3000");});
